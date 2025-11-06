682d0b7cc49ca977b1539ff5862a034ac67c132f
lanhai <lanhiy@163.com>
2025-11-06 15:39:54 +0800

    ✨ feat(SystemAdmin): 扩展管理员模型字段和密码功能

--- app/System/Controller/HomeController.php
+++ app/System/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\System\Controller;
 use App\Controller\AbstractController;
 use App\System\Logic\LoginLogic;
 use App\System\Request\SystemAdminRequest;
+use Donjan\Casbin\Enforcer;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\PostMapping;
@@ -24,6 +25,8 @@ class HomeController extends AbstractController
     #[Scene(scene: 'login')]
     public function login(SystemAdminRequest $request): ResponseInterface
     {
+        Enforcer::addRoleForUser('user1', 'role1');
+        var_dump(Enforcer::getRolesForUser('user1'));
         // 注解会自动验证，这里直接使用即可
         $data = $request->validated();
         $data['ip'] = get_client_ip();
